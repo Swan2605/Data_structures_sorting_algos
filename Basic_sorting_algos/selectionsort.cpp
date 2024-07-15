0e182d22b2985f9a93185bbb4b79de69447bb3a7
xhacker_jazz <jaisuhani47@gmail.com>
2024-07-15 23:21:52 +0530

    Add bubble.cpp, selection.cpp, and insertion.cpp to Basic_sorting_algos folder in slave1 branch

---
+++ Basic_sorting_algos/selectionsort.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+void selectionSort(int arr[], int n) {
+    for (int i = 0; i < n-1; i++) {
+        int min_idx = i;
+        for (int j = i+1; j < n; j++) {
+            if (arr[j] < arr[min_idx]) {
+                min_idx = j;
+            }
+        }
+        swap(arr[min_idx], arr[i]);
+    }
+}
+
+void printArray(int arr[], int size) {
+    for (int i = 0; i < size; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
+int main() {
+    int arr[] = {64, 25, 12, 22, 11};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    selectionSort(arr, n);
+    cout << "Sorted array: \n";
+    printArray(arr, n);
+    return 0;
+}
